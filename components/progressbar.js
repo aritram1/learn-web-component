233e30c6c650db05eeb3117ffdeb87dc52136ffd
aritra <aritram1@gmail.com>
2021-02-21 22:24:58 +0530

    Ended work for now

--- components/progressbar.js
+++ components/progressbar.js
@@ -3,51 +3,67 @@ class progressbar extends HTMLElement{
     constructor(){
         super();
         
-        //Create shadow root and copy template content to shadowRoot
+        //Create shadow root and add style element
         const shadow = this.attachShadow({mode : 'open'});
-            
-        let container = document.querySelector('#container');
-        console.log('container' + container);
-        shadow.appendChild(container.cloneNode(true));
+        let style = document.createElement('style');
+        style.setAttribute('src', './progressbar.css');
+        shadow.appendChild(style);
 
         //Create the progress bar div
         let bar = document.createElement('div');
+        bar.style.width = `${this.percentage}px`;
+        bar.style.height = '20px';
+        bar.style.backgroundColor = 'blue';
+        bar.setAttribute('id','bar');
         bar.classList.add('bar');
         shadow.appendChild(bar);
-
-        // Create style and add in component
-        // now being done declaratively. Hence commented out.
-        // See the template import for details.
-        // let style = document.createElement('style');
-        // style.setAttribute('src', './progressbar.css');
-        // shadow.appendChild(style);    
+        
+        // Create and add control
+        let inp = document.createElement('input');
+        inp.setAttribute('type', 'number');
+        let that = this;
+        inp.addEventListener('change', (ev)=>{
+            let p = ev.target.value;
+            console.log('I am clicked! Value being sent in event : ' + p);
+            //this.dispatchEvent(new CustomEvent('progressBarChange'), {v : p});
+            //that.percentage = p;
+            const bar = this.shadowRoot.getElementById('bar');
+            bar.style.width = `${p <= 100 ? p : 100}px`;
+        });
+        shadow.appendChild(inp);
     }
 
     connectedCallback(){
-        const bar = document.querySelector('.bar');
-        let width = this.percentage <= 100 ? this.percentage : 100;
-        bar.style.width = `${width} px`;
         console.log('Inside connetedCallback');
-        this.attachEventHandlers(this);
     }
 
-    attachEventHandlers(element){
-        console.log('Inside attachHandlers->');
-        console.log(element.shadowRoot);
+    static get observedAttributes() {
+        return ['percentage'];
     }
-
-    //b = document.querySelector();
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`${name}'s value has been changed from ${oldValue} to ${newValue}`);
+        if(name == 'percentage'){
+            const bar = this.shadowRoot.getElementById('bar');
+            let width = newValue <= 100 ? newValue : 100;
+            bar.style.width = `${width}px`;
+        }
+    }
 }
 customElements.define('progress-bar', progressbar);
 
+function createAndAddButton(element){
+    //Add the input element to control the progress bar 
+    // let inp = document.createElement('input');
+    // inp.setAttribute('type', 'number');
+    // inp.addEventListener('change', (e)=>{
+    //     console.log('I am clicked! Value being sent in event : ' + e.target.value);
+    //     this.dispatchEvent(new CustomEvent('progressBarChange'), {v : e.target.value});
+    //     element.percentage = e.target.value;
+    // });
+    // element.shadowRoot.appendChild(inp);
 
-//////////////////////////////////////////////////////////////////////OUTSIDE SHADOW ////////////////////////////
-/*
-The progress bar element can be created from javascript from three places. 
-    1. From the components <script> tag.
-    2. From the components js file (like here)
-    3. From the place where  it is being used.
-*/
-// //create the progress bar
-// const progressbar = document.createElement('progress-bar');
-// document.body.append(progressbar);
+    //TBD
+    // element.addEventListener('progressBarChange', (ev)=>{
+    //     console.log('progressbar change event handled. value received : ' + ev.detail.v);
+    // });
+}
